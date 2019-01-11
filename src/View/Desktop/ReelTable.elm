9dcc40473e453f7b7a7b0666e3b12f1208384da2
kfrn <knfrances@gmail.com>
2019-01-12 11:57:54 +1300

    13: Desktop reelTable into separate file

---
+++ src/View/Desktop/ReelTable.elm
@@ -0,0 +1,229 @@
+module View.Desktop.ReelTable exposing (desktopReelTable)
+
+import AppSettings exposing (SystemOfMeasurement(..))
+import Audio.Model exposing (Direction, Passes, allAudioConfigs, allDiameters, allRecordingSpeeds, allThicknesses, diameterDisplayName, speedDisplayName, tapeThicknessDisplayName)
+import Audio.Reel.Model exposing (Footage, Reel, footageToInt, formatTime, fullDuration, overallDuration, reelLengthInFeet, singleReelDuration)
+import AudioFile exposing (FileType(..), allFileTypes, fileTypeName, totalFilesize)
+import Html exposing (..)
+import Html.Attributes exposing (class, classList, disabled, id, placeholder)
+import Html.Events exposing (onClick, onInput)
+import Maybe.Extra exposing (isNothing)
+import Messages exposing (Msg(..))
+import Model exposing (Model)
+import Translate exposing (AppString(..), Language(..), audioConfigDisplayName, directionString, translate)
+import Uuid
+import View.Helpers exposing (renderSelect)
+
+
+desktopReelTable : Model -> Html Msg
+desktopReelTable model =
+    let
+        total =
+            if List.length model.reels > 0 then
+                [ totalRow model.language model.reels model.fileType ]
+
+            else
+                []
+
+        reelRows =
+            List.map (reelRow model.system model.language) model.reels
+    in
+    table [ class "table is-fullwidth" ]
+        ([ thead [] [ selectorRow model ]
+         , tbody [] reelRows
+         ]
+            ++ total
+        )
+
+
+selectorRow : Model -> Html Msg
+selectorRow model =
+    let
+        invalidQuantity =
+            isNothing model.selectorValues.quantity || model.selectorValues.quantity == Just 0
+    in
+    tr [ class "table-header main-row" ]
+        [ th [ class "pc10" ]
+            [ div [ class "select-heading left-offset is-size-6" ] [ text <| translate model.language TypeStr ]
+            , div [ class "select is-small" ]
+                [ renderSelect
+                    (translate model.language <| audioConfigDisplayName model.selectorValues.audioConfig)
+                    ChangeAudioConfig
+                    (\a -> translate model.language <| audioConfigDisplayName a)
+                    allAudioConfigs
+                ]
+            ]
+        , th [ class "pc10" ]
+            [ div [ class "select-heading is-size-6" ] [ text <| translate model.language DiameterStr ]
+            , div [ class "select is-small" ]
+                [ renderSelect
+                    (diameterDisplayName model.system model.selectorValues.diameter)
+                    ChangeDiameterInInches
+                    (diameterDisplayName model.system)
+                    allDiameters
+                ]
+            ]
+        , th [ class "pc10" ]
+            [ div [ class "select-heading left-offset is-size-6" ] [ text <| translate model.language ThicknessStr ]
+            , div [ class "select is-small" ]
+                [ renderSelect
+                    (tapeThicknessDisplayName model.selectorValues.tapeThickness)
+                    ChangeTapeThickness
+                    tapeThicknessDisplayName
+                    allThicknesses
+                ]
+            ]
+        , th [ class "pc10" ]
+            [ div [ class "select-heading left-offset is-size-6" ] [ text <| translate model.language SpeedStr ]
+            , div [ class "select is-small" ]
+                [ renderSelect
+                    (speedDisplayName model.system model.selectorValues.recordingSpeed)
+                    ChangeRecordingSpeed
+                    (speedDisplayName model.system)
+                    allRecordingSpeeds
+                ]
+            ]
+        , th [ class "pc5 has-text-centered" ]
+            [ div [ class "select-heading is-size-6" ] [ text <| translate model.language QuantityStr ]
+            , input
+                [ classList [ ( "input is-small", True ), ( "is-danger", invalidQuantity ) ]
+                , id "quantity"
+                , placeholder "#"
+                , onInput UpdateQuantity
+                ]
+                []
+            ]
+        , th [ class "pc25" ] [ div [ class "select-heading left-offset is-size-6" ] [ text <| translate model.language InfoHeaderStr ] ]
+        , th [ class "pc25" ] [ div [ class "select-heading left-offset is-size-6" ] [ text <| translate model.language DurationStr ] ]
+        , th [ class "pc5 has-text-centered" ]
+            [ div [ class "select-heading invisible" ] [ text "." ]
+            , button
+                [ class "button is-small", onClick AddReel, disabled invalidQuantity ]
+                [ span [ class "icon" ]
+                    [ i [ class "fa fa-plus" ] []
+                    ]
+                ]
+            ]
+        ]
+
+
+reelRow : SystemOfMeasurement -> Language -> Reel -> Html Msg
+reelRow system language reel =
+    let
+        deleteRowButton reelID =
+            button [ class "button is-small", onClick <| DeleteReel reelID ]
+                [ span [ class "icon" ]
+                    [ i [ class "fa fa-trash" ] []
+                    ]
+                ]
+
+        footage =
+            reelLengthInFeet reel
+    in
+    tr [ id (Uuid.toString reel.id) ]
+        [ td [] [ text <| translate language <| audioConfigDisplayName reel.audioConfig ]
+        , td [] [ text <| diameterDisplayName system reel.diameter ]
+        , td [] [ text <| tapeThicknessDisplayName reel.tapeThickness ]
+        , td [] [ text <| speedDisplayName system reel.recordingSpeed ]
+        , td [ class "has-text-centered" ] [ text <| String.fromInt reel.quantity ]
+        , td []
+            [ directionAndPassCount language reel.directionality reel.passes
+            , lengthInfo system language footage
+            ]
+        , td [] (durationData language reel)
+        , td [] [ deleteRowButton reel.id ]
+        ]
+
+
+directionAndPassCount : Language -> Direction -> Passes -> Html Msg
+directionAndPassCount language direction passes =
+    let
+        passText =
+            translate language (NumPassesStr passes)
+
+        fullDirectionString =
+            translate language (directionString direction)
+    in
+    div [] [ text (fullDirectionString ++ ": " ++ passText) ]
+
+
+lengthInfo : SystemOfMeasurement -> Language -> Footage -> Html Msg
+lengthInfo system language footage =
+    let
+        ft =
+            footageToInt footage
+
+        metricLength =
+            toFloat ft * 0.3048
+
+        lengthText =
+            case system of
+                Imperial ->
+                    String.fromInt ft ++ "ft"
+
+                Metric ->
+                    String.fromFloat metricLength ++ "m"
+
+        perReel =
+            translate language (PerReelStr lengthText)
+    in
+    div [] [ text perReel ]
+
+
+durationData : Language -> Reel -> List (Html Msg)
+durationData language reel =
+    [ div []
+        [ text <|
+            translate language (PerReelStr <| String.fromFloat (singleReelDuration reel) ++ "min")
+        ]
+    , div []
+        [ text <|
+            formatTime (fullDuration reel)
+                ++ " "
+                ++ translate language TotalStr
+        ]
+    ]
+
+
+totalRow : Language -> List Reel -> FileType -> Html Msg
+totalRow language reels fileType =
+    let
+        totalMins =
+            overallDuration reels
+
+        formattedTime =
+            formatTime totalMins
+    in
+    tfoot []
+        [ tr [ class "main-row" ]
+            [ td [ class "has-text-weight-bold is-size-6" ] [ text <| translate language TotalStr ]
+            , td [] []
+            , td [] []
+            , td [] []
+            , td [] []
+            , td [ class "duration-filesize" ]
+                [ div [ class "total-top has-text-weight-bold" ] [ text <| translate language DurationStr ]
+                , div []
+                    [ span [ class "has-text-weight-bold" ] [ text <| translate language FileSizeStr ]
+                    , div [ class "select is-small", id "filetype" ]
+                        [ renderSelect
+                            (fileTypeName fileType)
+                            ChangeFileType
+                            fileTypeName
+                            allFileTypes
+                        ]
+                    ]
+                ]
+            , td []
+                [ div [ class "total-top" ] [ text <| translate language (DurationSummaryStr totalMins formattedTime) ]
+                , div [] [ text <| translate language (SizeInMegaBytesStr <| totalFilesize fileType reels) ]
+                ]
+            , td []
+                [ button [ class "button is-small", onClick StartExport ]
+                    [ span [ class "icon" ]
+                        [ i [ class "fa fa-download" ] []
+                        ]
+                    ]
+                ]
+            ]
+        ]
