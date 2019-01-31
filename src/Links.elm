7a0610dd356929d83761e1306fd1572a9a1236d9
kfrn <knfrances@gmail.com>
2019-01-31 20:34:02 +1300

    Links: reorganise attributed links
    Rather than marrying the link + attribution in the view, I'm now combining them in a function within the Links module.
    This also makes the view cleaner.

--- src/Links.elm
+++ src/Links.elm
@@ -1,46 +1,38 @@
-module Links exposing (LinkData(..), LinkName(..), link, linkData)
+module Links exposing (LinkName(..), attributedLink, link)
 
 import Html exposing (Html, a, i, span, text)
 import Html.Attributes exposing (class, href)
 
 
-linkData : LinkData -> String
-linkData data =
-    case data of
-        IASAGuidelinesENData ->
-            " (2nd ed.), IASA Technical Committee, 2009"
+attributedLink : LinkName -> List (Html msg)
+attributedLink linkName =
+    case linkName of
+        Estimating ->
+            [ link Estimating, text ", Joshua Ranger, AVPreserve, 12/2014" ]
 
-        IASAGuidelinesFRData ->
-            " (2ème ed.), IASA Comité Technique, 2009, tr. 2015"
+        Facet ->
+            [ link Facet, text " (PDF), Mike Casey, Indiana University, 2007" ]
 
-        IASAGuidelinesITData ->
-            ", IASA Comitato Technico, 2004, tr. 2007"
+        IASAGuidelinesEN ->
+            [ link IASAGuidelinesEN, text " (2nd ed.), IASA Technical Committee, 2009" ]
 
-        IASAMagLinkENData ->
-            ", IASA Technical Committee, 2014"
+        IASAGuidelinesFR ->
+            [ link IASAGuidelinesFR, text " (2ème ed.), IASA Comité Technique, 2009, tr. 2015" ]
 
-        IASAMagLinkITData ->
-            ", IASA Comitato Technico, 2014, tr. 2016"
+        IASAGuidelinesIT ->
+            [ link IASAGuidelinesIT, text ", IASA Comitato Technico, 2004, tr. 2007" ]
 
-        RangerData ->
-            ", Joshua Ranger, AVPreserve, 12/2014"
+        IASAMagLinkEN ->
+            [ link IASAMagLinkEN, text ", IASA Technical Committee, 2014" ]
 
-        RichardHessData ->
-            ", Richard Hess, 03/2006"
+        IASAMagLinkIT ->
+            [ link IASAMagLinkIT, text ", IASA Comitato Technico, 2014, tr. 2016" ]
 
-        CaseyData ->
-            " (PDF), Mike Casey, Indiana University, 2007"
+        RichardHess ->
+            [ link RichardHess, text ", Richard Hess, 03/2006" ]
 
-
-type LinkData
-    = IASAGuidelinesENData
-    | IASAGuidelinesFRData
-    | IASAGuidelinesITData
-    | IASAMagLinkENData
-    | IASAMagLinkITData
-    | RangerData
-    | RichardHessData
-    | CaseyData
+        _ ->
+            []
 
 
 link : LinkName -> Html msg
@@ -75,10 +67,10 @@ link name =
             a [ href "http://www.aib.it/aib/editoria/2007/pub172.htm" ] [ text "IASA TC-04: Linee guida per la produzione e la preservazione di oggetti audio digitali" ]
 
         IASAMagLinkEN ->
-            a [ href "https://www.iasa-web.org/tc05/2211-magnetic-tapes" ] [ text "Magnetic tapes, IASA TC-05: Handling and Storage of Audio and Video Carriers" ]
+            a [ href "https://www.iasa-web.org/tc05/2211-magnetic-tapes" ] [ text "IASA TC-05: Handling and Storage of Audio and Video Carriers - Magnetic tapes" ]
 
         IASAMagLinkIT ->
-            a [ href "https://www.iasa-web.org/tc05-it/2211-nastri-magnetici" ] [ text "Nastri magnetici, IASA TC-05: Gestione e archiviazione dei supporti audio e video" ]
+            a [ href "https://www.iasa-web.org/tc05-it/2211-nastri-magnetici" ] [ text "IASA TC-05: Gestione e archiviazione dei supporti audio e video - Nastri magnetici" ]
 
         ORADCalc ->
             a [ href "https://www.avpreserve.com/open-reel-audio-duration-calculator/" ] [ text "Open Reel Audio Duration Calculator" ]
--- src/View/Info.elm
+++ src/View/Info.elm
@@ -3,7 +3,7 @@ module View.Info exposing (infoParagraph, linksSection, questionSection)
 import AppSettings exposing (PageView(..), SystemOfMeasurement(..))
 import Html exposing (Html, div, h2, li, p, strong, text, ul)
 import Html.Attributes exposing (class)
-import Links exposing (LinkData(..), LinkName(..), link, linkData)
+import Links exposing (LinkName(..), attributedLink)
 import Messages exposing (Msg(..))
 import Translate exposing (AppString(..), Language(..), infoPara, translate)
 import View.Helpers exposing (wrapSectionInLevelDiv)
@@ -46,36 +46,27 @@ questionSection language =
 linksSection : Language -> Html Msg
 linksSection language =
     let
-        iasaTC04Link =
+        listItem textContent =
+            li [] textContent
+
+        iasaLinks =
             case language of
                 EN ->
-                    [ li [] [ link IASAGuidelinesEN, text <| linkData IASAGuidelinesENData ] ]
+                    [ listItem <| attributedLink IASAMagLinkEN, listItem <| attributedLink IASAGuidelinesEN ]
 
                 FR ->
-                    [ li [] [ link IASAGuidelinesFR, text <| linkData IASAGuidelinesFRData ] ]
+                    [ listItem <| attributedLink IASAGuidelinesFR ]
 
                 IT ->
-                    [ li [] [ link IASAGuidelinesIT, text <| linkData IASAGuidelinesITData ] ]
-
-        iasaTC05Link =
-            case language of
-                EN ->
-                    [ li [] [ link IASAMagLinkEN, text <| linkData IASAMagLinkENData ] ]
-
-                FR ->
-                    []
-
-                IT ->
-                    [ li [] [ link IASAMagLinkIT, text <| linkData IASAMagLinkITData ] ]
+                    [ listItem <| attributedLink IASAMagLinkIT, listItem <| attributedLink IASAGuidelinesIT ]
     in
     div []
         [ h2 [ class "subtitle" ] [ text <| translate language UsefulLinksStr ]
         , ul []
-            ([ li [] [ link RichardHess, text <| linkData RichardHessData ] ]
-                ++ iasaTC05Link
-                ++ iasaTC04Link
-                ++ [ li [] [ link Estimating, text <| linkData RangerData ]
-                   , li [] [ link Facet, text <| linkData CaseyData ]
+            (iasaLinks
+                ++ [ listItem <| attributedLink RichardHess
+                   , listItem <| attributedLink Estimating
+                   , listItem <| attributedLink Facet
                    ]
             )
         ]
